12de0b88f46394cd6e9b4d89335fd45384a4eeb3
stepangolovko <stepagolovko1993@gmail.com>
2019-10-27 02:32:13 +0500

    add actioncable logic on index page for rate upd

---
+++ app/channels/web_rate_update_channel.rb
@@ -0,0 +1,9 @@
+class WebRateUpdateChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "web_rate_update_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- app/workers/update_rate_worker.rb
+++ app/workers/update_rate_worker.rb
@@ -3,6 +3,8 @@ class UpdateRateWorker
 
   def perform(name, count)
     ParseRateService.new.update_rate
+    rate = YamlService.new.get(:current_rate)
+    ActionCable.server.broadcast("web_rate_update_channel", content: rate)
     UpdateRateWorker.perform_at(1.hour, "Update Rate", 1)
   end
 end
