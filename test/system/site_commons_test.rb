94113a7f3c92f24e4881a69a39f4093ce5212600
stepangolovko <stepagolovko1993@gmail.com>
2019-10-29 23:50:40 +0500

    fix no webpack command error and create sys test

---
+++ test/system/site_commons_test.rb
@@ -0,0 +1,8 @@
+require "application_system_test_case"
+
+class SiteCommonsTest < ApplicationSystemTestCase
+  test "visit the index" do
+    visit root_path
+    page.has_content?("Текущий курс доллара:")
+  end
+end
